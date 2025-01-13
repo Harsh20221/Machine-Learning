c91681a27bb22672226697a4f77be1b4183d14dc
Harsh <harsh20221@gmail.com>
2025-01-14 01:44:17 +0530

    More Changes and conversions

--- Classification/Logistic Regression/Logistic_regression.py
+++ Classification/Logistic Regression/Logistic_regression.py
@@ -14,4 +14,16 @@ from sklearn.preprocessing import StandardScaler
 sc=StandardScaler()
 x_train=sc.fit_transform(x_train)##/ Always fit transform is used for feature scaling 
 x_test=sc.fit_transform(x_test)##! Make sure you apply feature scaling on x train and  x test since we have already split our data and on x only using same scaler because we are applying only on x , if we would have been also applying it on y then we would have taken seperate scaler for y too 
-print(x_train)
+###* Applying our classification model 
+from sklearn.linear_model import LogisticRegression
+classifier= LogisticRegression(random_state=0)
+classifier.fit(x_train,y_train)##? Here we are fitting the model into our dataset
+##* Predicting for a single value 
+classifier.predict(sc.transform([[30,87000]])) ##!!Make sure you always uise sc.transform when you are making prediction on feature scaled data
+##!! Make sure you write the prediction parameters inside a 2d array if you have more than one parameter for prediction 
+##? In the above code snippet we are predicting fore someone with age=30 and salary = 87000 whether he/she will buy the car or not 
+####* Predicting the Test Set Results 
+y_predict=classifier.predict(x_test)
+print(y_predict) ##? This will print the prediction 
+print (np.concatenate( (y_predict. reshape (len(y_predict) ,1), y_test.reshape(len(y_test) ,1)), 1))##?The active selection is a Python statement that prints the result of concatenating two arrays, y_predict and y_test, along the second axis (columns). This is achieved using the np.concatenate function from the NumPy library, which joins a sequence of arrays along an existing axis.                                                    
+##?First, both y_predict and y_test arrays are reshaped to be two-dimensional with a single column. The reshape method is used for this purpose, where len(y_predict) and len(y_test) specify the number of rows, and 1 specifies the single column. This transformation ensures that both arrays have compatible shapes for concatenation along the specified axis
