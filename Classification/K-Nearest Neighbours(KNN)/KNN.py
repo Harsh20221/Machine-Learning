c1370ec3bcebb8e8991dc18d0b763491bea45125
Harsh <harsh20221@gmail.com>
2025-01-14 05:25:52 +0530

    more changes

--- Classification/K-Nearest Neighbours(KNN)/KNN.py
+++ Classification/K-Nearest Neighbours(KNN)/KNN.py
@@ -33,43 +33,29 @@ accuracy_score(y_test, y_pred)##/ same parameters will be used for the accduracy
 ##* Visualizing Training Set (Code not too important )
 from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(x_train), y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.5),
-                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.5))
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.5),
+                     np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=0.5))
 plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(['#FA8072', '#1E90FF']))
+             alpha=0.75, cmap=ListedColormap(['#FA8072', '#1E90FF']))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(['#FA8072', '#1E90FF'])(i), label = j)
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(['#FA8072', '#1E90FF'])(i), label=j)
 plt.title('K-NN (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
-
 ##* Visualizimng Test Set (Code not too important )
-
-from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(x_test), y_test
-# Create a grid of points
-X1, X2 = np.meshgrid(
-    np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.5),
-    np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.5)
-)
-# Predict for each point on the grid
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.5),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.5))
 Z = classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape)
-# Plot the decision boundary
-plt.contourf(X1, X2, Z, alpha=0.75, cmap = ListedColormap(['#FA8072', '#1E90FF']) )
+plt.contourf(X1, X2, Z, alpha=0.75, cmap=ListedColormap(['#FA8072', '#1E90FF']))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
-# Define colors for scatter plot
 colors = ['#FA8072', '#1E90FF']
-# Plot the test set points
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(
-        X_set[y_set == j, 0], X_set[y_set == j, 1],
-        color=colors[i], label=j
-    )
-# Add titles and labels
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], color=colors[i], label=j)
 plt.title('K-NN (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
