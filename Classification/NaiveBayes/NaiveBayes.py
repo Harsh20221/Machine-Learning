e30b4851475bfff096c238d7d06e2725f93acc05
Harsh <harsh20221@gmail.com>
2025-01-17 16:44:18 +0530

    Added Plots

--- Classification/NaiveBayes/NaiveBayes.py
+++ Classification/NaiveBayes/NaiveBayes.py
@@ -18,5 +18,49 @@ classifier=GaussianNB()
 classifier.fit(x_train,y_train)
 y_pred=classifier.predict(x_test)
 print(classifier.predict(sc.transform([[30,87000]])))
-print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1)) ###!!This is the correct syntax for printing the predictions 
+print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1)) ###!!This is the correct syntax for printing a particular prediction 
 
+##*Training set PLOT 
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(x_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(['red', 'green']))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(['red', 'green'])(i), label = j)
+plt.title('Naive Bayes (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+###*Test Set Plot 
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(x_test), y_test
+# Create a grid of points
+X1, X2 = np.meshgrid(
+    np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.25),
+    np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.25)
+)
+# Predict for each point on the grid
+Z = classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape)
+# Plot the decision boundary
+plt.contourf(X1, X2, Z, alpha=0.75, cmap = ListedColormap(['red', 'green']) )
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+# Define colors for scatter plot
+colors = ['red', 'green']
+# Plot the test set points
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        X_set[y_set == j, 0], X_set[y_set == j, 1],
+        color=colors[i], label=j
+    )
+# Add titles and labels
+plt.title('Naive Bayes (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
