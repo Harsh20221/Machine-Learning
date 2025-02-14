18315e22f0c92b5def64cbed77e02459c57f00f8
Harsh <harsh20221@gmail.com>
2025-02-14 10:03:30 +0530

    Created a Bag of Words Model

--- Natural Language Processing/NLP.py
+++ Natural Language Processing/NLP.py
@@ -5,9 +5,13 @@ dataset=pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3) ###? The 
 ###* CLEANING THE TEXT FROM THE DATASET SO THAT OUR MODEL CAN PROCESS
 ##/We'll be removing all non-relevant words from the dataset
 import re   ###?The stemming will simplify the sentences , Whether you say " I loved this  restaurant or I love this restaurant , the meaning remains  the same so we transform that loved to love
+
+#####!!!!VERY IMP STEP , HERE WE ARE BYPASSING SSL CERTIFICATE REQUIREMENTS , IF NOT DONE IT'LL SHOW SSL ERRORS 
 import ssl
 import certifi
 ssl._create_default_https_context = ssl._create_unverified_context
+
+
 import nltk    ###?The initial step in our cleaning process will involve removing all punctuation marks from our code
 nltk.download('stopwords')
 from nltk.corpus import stopwords###/This set of stopwords will contain all the words of english and based on this set we will omit other words for stemming 
@@ -24,8 +28,11 @@ for i in range(0,1000):
     corpus.append(reviews)##? Finally appending all the clean reviews to the corpus array 
 print(corpus)
  
-
-
+####* Creating Bag of Words Model 
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+x=cv.fit_transform(corpus).toarray()
+y=dataset.iloc[:,-1].values
 
 
 
