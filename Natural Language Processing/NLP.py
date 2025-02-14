9234b58f0a4e8aeea0b3c027b5d2ea9bd296434f
Harsh <harsh20221@gmail.com>
2025-02-14 10:29:27 +0530

    printed accuracy score

--- Natural Language Processing/NLP.py
+++ Natural Language Processing/NLP.py
@@ -46,7 +46,7 @@ print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
-accuracy_score(y_test, y_pred)
+print(accuracy_score(y_test, y_pred))
 
 
 
