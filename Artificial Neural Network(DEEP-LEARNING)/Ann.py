fba6ff9b73ce7f78bf7752430f195355c35f8772
Harsh Kumar <harsh20221@gmail.com>
2025-03-18 02:55:04 +0530

    comments

--- Artificial Neural Network(DEEP-LEARNING)/Ann.py
+++ Artificial Neural Network(DEEP-LEARNING)/Ann.py
@@ -14,14 +14,16 @@ x=np.array(ct.fit_transform(x))
 from sklearn.preprocessing import LabelEncoder###? This will be used to perform encoding(Convert to numbers) to the Male/Female Column 
 le=LabelEncoder()
 x[:,2]=le.fit_transform(x[:,2])
- 
+ ###*SPLIT INTIO TRAINING AND TEST SET
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,train_size=0.8,random_state=0)
-
+##* FEATURE SCALING
 from sklearn.preprocessing import StandardScaler
 sc=StandardScaler()
 x_train=sc.fit_transform(x_train)
 x_test=sc.transform(x_test) ###?When sc.transform(x_test) is called, the test data is scaled using the same mean and standard deviation that were computed from the training set. This ensures that the test data is transformed in the same way as the training data.
+
+
 
 
 
