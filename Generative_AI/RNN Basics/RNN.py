825ad69bff39cd60d5e453981bf958d8dba45712
Harsh <harsh20221@gmail.com>
2025-08-18 04:21:04 +0530

    Changes to the RNN Dataset

--- Generative_AI/RNN Basics/RNN.py
+++ Generative_AI/RNN Basics/RNN.py
@@ -1,1 +1,43 @@
-from tensorflow.keras.preprocessing import one_hot  # type: ignore
+
+###* Initializing the Simple RNN and processing the data using RNN---Here in tghis file we are working on the IMDB dataset and we areb using RNN Learning principle to classify the reviews into  Positive or Negative 
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb #type:ignore
+from tensorflow.keras.preprocessing import sequence #type:ignore
+from tensorflow.keras.models import Sequential #type:ignore
+from tensorflow.keras.layers import Embedding,SimpleRNN,Dense #type:ignore
+max_features=10000 #?Vocabulary size
+(X_train,Y_train),(X_test,Y_test)=imdb.load_data(num_words=max_features)
+print(f'Training Data Shape: {X_train.shape}, Training labels shape : {Y_train.shape}')##? the f after print makes this a strin g literal , Here In this step we are printing the shape of Training data Parameters that it'll be trained and parameter that  it'll predict 
+print(f'Testing Data Shape: {X_test.shape}, Testing labels shape : {Y_test.shape}')##? Doing the above for test data as well 
+print(X_train[0],Y_train[0])
+from tensorflow.keras.preprocessing import sequence #type:ignore
+max_length=200  #!! Reduced from 500 to 200 as most reviews don't need 500 words to convey sentiment
+X_train=sequence.pad_sequences(X_train, maxlen=max_length, truncating='post', padding='post')
+X_test=sequence.pad_sequences(X_test, maxlen=max_length, truncating='post', padding='post')
+##* Train Simple RNN 
+model = Sequential()
+model.add(Embedding(max_features, 128, input_length=max_length))
+model.add(SimpleRNN(128, activation='relu'))
+model.add(Dense(1, activation='sigmoid')) ###? The dense layer is like a output layer 
+
+# Compile the model with appropriate optimizer and loss function for binary classification
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Build the model with explicit input shape
+model.build((None, max_length))
+
+# Show model summary
+model.summary()
+
+from tensorflow.keras.callbacks import EarlyStopping #type:ignore
+earlystopping=EarlyStopping(monitor='val_loss',patience=5,restore_best_weights=True)
+
+history=model.fit(
+    X_train,Y_train,
+    epochs=10,
+    batch_size=128,  # !!Increased batch size for faster training
+    validation_split=0.2,
+    callbacks=[earlystopping]
+)
+
---
+++ Generative_AI/RNN Basics/RNNexample.py
@@ -0,0 +1,33 @@
+from tensorflow.keras.preprocessing.text import one_hot  # type: ignore
+text=['the glass of milk',
+     'the glass of juice',
+     'the cup of tea',
+    'I am a good boy',
+     'I am a good developer',
+     'understand the meaning of words',
+     'your videos are good',]
+voc_size=10000
+###* ONE HOT ENCODING To conevrt Words into Vectors
+one_hotrepr = []
+for sentence in text:
+    encoded = one_hot(sentence, voc_size)
+    one_hotrepr.append(encoded)                                       
+#print(one_hotrepr)
+from tensorflow.keras.layers import Embedding #type:ignore  
+from tensorflow.keras.utils import pad_sequences#type:ignore 
+from tensorflow.keras.models import Sequential#type:ignore
+import numpy as np
+###* CREATING A EMBEDDING LAYER 
+###?It converts the sparse one-hot vectors (vocabulary size of 100000) into dense vectors of size 10 (dim=10)
+###?he embeddings can capture semantic relationships between words. For example, "cup" and "glass" might end up with similar embedding vectors because they are both containers
+text_length=8
+embedded_docs=pad_sequences(one_hotrepr,padding='pre',maxlen=text_length) ##?The Padsequences will normalize all words to be of same size , it'll either add zeroes in the start or the end so that all words are of same size  
+#print(embedded_docs)
+dim=10
+model = Sequential()
+model.add(Embedding(voc_size, dim, input_length=text_length))
+# Build the model with explicit input shape
+model.build((None, text_length))
+model.compile('adam', 'mse')
+model.summary()
+model.predict(embedded_docs)
