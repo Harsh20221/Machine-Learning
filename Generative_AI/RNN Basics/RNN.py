5d2c23c7b32a52702d845468768903b22c2047ed
Harsh <harsh20221@gmail.com>
2025-08-19 15:06:24 +0530

    fixed

--- Generative_AI/RNN Basics/RNN.py
+++ Generative_AI/RNN Basics/RNN.py
@@ -18,7 +18,7 @@ X_train=sequence.pad_sequences(X_train, maxlen=max_length, truncating='post', pa
 X_test=sequence.pad_sequences(X_test, maxlen=max_length, truncating='post', padding='post')
 
 ###*THIS WILL CHECK FOR AVAILABLE MODELS AND IF found it'll 
-model_path = 'simple_Rnnew_imdb.h5'
+model_path = 'simple_Rnnnew_imdb.h5'
 
 if os.path.exists(model_path):
     print(f"Loading existing model from {model_path}...")
@@ -83,7 +83,7 @@ def predict_sentiment(review):
 
 # Step 4: User Input and Prediction
 # Example review for prediction
-example_review = "This movie was bad The acting was also bad  and the plot was bad."
+example_review = "This movie was bad"
 
 sentiment,score=predict_sentiment(example_review)
 
