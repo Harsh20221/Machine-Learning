465a7f88e3ef5c28df667e7e4f7ee5dbc9fc951d
harsh <rprasad2004@gmail.com>
2026-06-10 01:18:02 +0530

    Initialized model and ran a cOMMAND

--- Generative_AI/Building_BasicLLm_Using_LCEL/Groktest.py
+++ Generative_AI/Building_BasicLLm_Using_LCEL/Groktest.py
@@ -7,6 +7,13 @@ groq_api_key=os.getenv("GROQ_API_KEY")
 ##* Importing the groq chat 
 from langchain_groq import ChatGroq
 ##* Initializing Model 
-model=ChatGroq(model="gemma:2b",groq_api_key=groq_api_key)
-print(model)
+model=ChatGroq(model="llama-3.1-8b-instant",groq_api_key=groq_api_key)
+#*ACTUal code
+from langchain_core.messages import HumanMessage,SystemMessage #? To specify the llm whcch message is provided by human being and which is a system message , System message Instructs the LLM how really it needs to behave or work
+messages=[
+    SystemMessage(content="Translate the Following From English to French "),
+    HumanMessage(content="Hello How are you ")
+]
+result = model.invoke(messages)
+print(result.content)
 
